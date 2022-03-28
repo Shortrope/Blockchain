c38a656c331fe969d876b8d521791603016244a8
Mario Kaack <mario@shortrope.com>
2022-03-27 20:24:52 -0700

    Python: Menu of options: Add tx, print chain, quit

--- python/blockchain.py
+++ python/blockchain.py
@@ -9,14 +9,28 @@ def add_value(transaction_amount, last_transaction):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_amount():
     return float(input("Your transaction amount please: "))
 
 
-add_value(get_user_input(), get_last_blockchain_value())
-add_value(get_user_input(), get_last_blockchain_value())
-add_value(get_user_input(), get_last_blockchain_value())
-add_value(get_user_input(), get_last_blockchain_value())
+def get_user_choice():
+    return input("Your choice: ")
 
-for block in blockchain:
-    print(block)
+
+def print_blockchain_elements():
+    for block in blockchain:
+        print(block)
+
+
+while True:
+    print("Choose Option:")
+    print("  a: Add transaction amount")
+    print("  o: Output chain")
+    print("  q: Quit")
+    choice = get_user_choice()
+    if choice == "a":
+        add_value(get_transaction_amount(), get_last_blockchain_value())
+    elif choice == "o":
+        print_blockchain_elements()
+    else:
+        break
