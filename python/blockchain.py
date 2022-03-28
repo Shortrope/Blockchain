679df10523ae82bec76bdc224bc363ec66c3042f
Mario Kaack <mario@shortrope.com>
2022-03-27 21:28:50 -0700

    Python: verify blockchain

--- python/blockchain.py
+++ python/blockchain.py
@@ -9,7 +9,7 @@ def get_last_blockchain_value():
 
 def add_transaction(transaction_amount, last_transaction):
     if last_transaction == None:
-        last_transaction = [1]
+        last_transaction = [0]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -26,15 +26,35 @@ def print_blockchain_elements():
         print(block)
 
 
+def verify_chain():
+    if len(blockchain) > 1:
+        for i in range(1, len(blockchain)):
+            if blockchain[i][0] != blockchain[i - 1]:
+                return False
+    return True
+
+
 while True:
     print("Choose Option:")
     print("  a: Add transaction amount")
     print("  o: Output chain")
+    print("  m: Manipulate chain")
     print("  q: Quit")
     choice = get_user_choice()
     if choice == "a":
         add_transaction(get_transaction_amount(), get_last_blockchain_value())
+        if not verify_chain():
+            print("INVALID CHAIN!!!\nDone!")
+            print_blockchain_elements()
+            break
     elif choice == "o":
         print_blockchain_elements()
+    elif choice == "m":
+        if len(blockchain) >= 1:
+            blockchain[0] = [-1]
+        if not verify_chain():
+            print("INVALID CHAIN!!!\nDone!")
+            print_blockchain_elements()
+            break
     else:
         break
