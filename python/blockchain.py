e9ed3f2a745ad4ca7aba56dc9cc48551250d3087
Mario Kaack <mario@shortrope.com>
2022-03-27 19:19:23 -0700

    Python: print each block in chain w for loop

--- python/blockchain.py
+++ python/blockchain.py
@@ -1,11 +1,11 @@
-blockchain = []
+blockchain = [[]]
 
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_value(transaction_amount, last_transaction):
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -13,9 +13,10 @@ def get_user_input():
     return float(input("Your transaction amount please: "))
 
 
-add_value(get_user_input())
+add_value(get_user_input(), get_last_blockchain_value())
 add_value(get_user_input(), get_last_blockchain_value())
 add_value(get_user_input(), get_last_blockchain_value())
 add_value(get_user_input(), get_last_blockchain_value())
 
-print(blockchain)
+for block in blockchain:
+    print(block)
