b914eb34fc50906c9dcdbbb4623497fad2dfd68c
Mario Kaack <mario@shortrope.com>
2022-04-09 12:09:28 -0700

    Python: add mine_block(), hash_block
    Update varify_chain to compare hashes
    
        modified:   blockchain.py

--- python/blockchain.py
+++ python/blockchain.py
@@ -1,4 +1,7 @@
-blockchain = []
+from tkinter import W
+
+genesis_block = {"previous_hash": "", "index": 0, "transactions": []}
+blockchain = [genesis_block]
 open_transactions = []
 owner = "Mak"
 
@@ -25,6 +28,23 @@ def get_user_choice():
     return input("Your choice: ")
 
 
+def hash_block(block):
+    # print("".join([str(block[k]) for k in block]))
+    return "".join([str(block[k]) for k in block])
+
+
+def mine_block():
+    last_block = blockchain[-1]
+    last_block_hash = hash_block(last_block)
+    # print(f"Last block hash: {last_block_hash}")
+    new_block = {
+        "previous_hash": last_block_hash,
+        "index": len(blockchain),
+        "transactions": open_transactions,
+    }
+    blockchain.append(new_block)
+
+
 def print_blockchain_elements():
     output = ""
     # output = ", ".join(map(str, blockchain))
@@ -35,9 +55,11 @@ def print_blockchain_elements():
 
 
 def verify_chain():
-    if len(blockchain) > 1:
-        for i in range(1, len(blockchain)):
-            if blockchain[i][0] != blockchain[i - 1]:
+    for i, block in enumerate(blockchain):
+        if i > 0:
+            print(f"prev hash: {block['previous_hash']}")
+            print(f"calc prev hash: {hash_block(blockchain[i-1])}")
+            if block["previous_hash"] != hash_block(blockchain[i - 1]):
                 return False
     return True
 
@@ -48,19 +70,22 @@ def main():
     while wait_for_input:
         print("Choose Option:")
         print("  a: Add transaction amount")
+        print("  m: Mine block")
         print("  p: Print chain")
-        print("  m: Manipulate chain")
+        print("  h: Hack chain")
         print("  q: Quit")
         choice = get_user_choice()
         if choice == "a":
             recipient, sender, amount = get_transaction()
             add_transaction(recipient, sender, amount)
             print(open_transactions)
+        elif choice == "m":
+            mine_block()
         elif choice == "p":
             print_blockchain_elements()
-        elif choice == "m":
+        elif choice == "h":
             if len(blockchain) >= 1:
-                blockchain[0] = [-1]
+                blockchain[0]["transactions"] = [-1]
         elif choice == "q":
             break
         else:
